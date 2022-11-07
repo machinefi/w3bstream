78e5084630ba7379cea3ef033c2d742143eb0f5d
huangzhiran <30522704+huangzhiran@users.noreply.github.com>
2022-11-07 18:33:45 +0800

    add http init

--- cmd/srv-applet-mgr/apis/monitor/root.go
+++ cmd/srv-applet-mgr/apis/monitor/root.go
@@ -9,5 +9,9 @@ var Root = kit.NewRouter(httptransport.Group("/monitor"))
 
 func init() {
 	Root.Register(kit.NewRouter(&CreateContractLog{}))
+	Root.Register(kit.NewRouter(&CreateChainTx{}))
+	Root.Register(kit.NewRouter(&CreateChainHeight{}))
 	Root.Register(kit.NewRouter(&RemoveContractLog{}))
+	Root.Register(kit.NewRouter(&RemoveChainTx{}))
+	Root.Register(kit.NewRouter(&RemoveChainHeight{}))
 }
