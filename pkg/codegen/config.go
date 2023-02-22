7291418c32f7e89935a8d8a859d7c21846499314
hunshenshi <289517357@qq.com>
2023-02-22 22:43:07 +0800

    add switch

--- pkg/codegen/config.go
+++ pkg/codegen/config.go
@@ -35,30 +35,30 @@ func (c *Config) SnippetInitChannel() {}
 
 func (c *Config) SnippetCompute() {}
 
-func (c *Config) SnippetCommVar(f *g.File) (snippets []g.Snippet) {
-	// filter func
-	//snippets = append(snippets, g.DeclVar(
-	//	g.Var(g.Bool, "res"),
-	//	g.Var(g.Slice(g.Byte), "ret"),
-	//	g.Var(g.Bool, "ok"),
-	//),
-	//)
-
-	// map func
-	snippets = append(snippets, g.DeclVar(
-		g.Var(g.Type(f.Use("models", "Customer")), "res"),
-		g.Var(g.Slice(g.Byte), "ret"),
-		g.Var(g.Bool, "ok"),
-	),
-	)
-
-	// groupBy
-	//snippets = append(snippets, g.DeclVar(
-	//	g.Var(g.String, "res"),
-	//	g.Var(g.Slice(g.Byte), "ret"),
-	//	g.Var(g.Bool, "ok"),
-	//),
-	//)
+func (c *Config) SnippetCommVar(f *g.File, op string) (snippets []g.Snippet) {
+	switch op {
+	case "filter":
+		snippets = append(snippets, g.DeclVar(
+			g.Var(g.Bool, "res"),
+			g.Var(g.Slice(g.Byte), "ret"),
+			g.Var(g.Bool, "ok"),
+		),
+		)
+	case "map":
+		snippets = append(snippets, g.DeclVar(
+			g.Var(g.Type(f.Use("models", "Customer")), "res"),
+			g.Var(g.Slice(g.Byte), "ret"),
+			g.Var(g.Bool, "ok"),
+		),
+		)
+	case "groupBy":
+		snippets = append(snippets, g.DeclVar(
+			g.Var(g.String, "res"),
+			g.Var(g.Slice(g.Byte), "ret"),
+			g.Var(g.Bool, "ok"),
+		),
+		)
+	}
 
 	return
 }
@@ -78,7 +78,19 @@ func (c *Config) SnippetInDeser(f *g.File) (snippets []g.Snippet) {
 	return
 }
 
-func (c *Config) SnippetInvokeWasm(f *g.File) (snippets []g.Snippet) {
+func (c *Config) SnippetInvokeWasm(f *g.File, op string) (snippets []g.Snippet) {
+	var doCode g.Snippet
+	switch op {
+	case "filter":
+		doCode = g.Return(g.Ident("res"))
+	case "map":
+		doCode = g.Return(g.Exprer("nil"),
+			g.Call(f.Use(pkgErrors, "New"), g.Valuer("the value does not support")),
+		)
+	case "groupBy":
+		doCode = g.Return(g.Exprer("error"))
+	}
+
 	snippets = append(snippets, g.Define(g.Var(nil, "code")).
 		By(
 			g.CallWith(
@@ -89,21 +101,24 @@ func (c *Config) SnippetInvokeWasm(f *g.File) (snippets []g.Snippet) {
 
 		g.If(g.Exprer("code < 0")).
 			Do(
-				// filter
-				//g.Return(g.Ident("res")),
-
-				// map
-				g.Return(g.Exprer("nil"),
-					g.Call(f.Use(pkgErrors, "New"), g.Valuer("the value does not support")),
-				),
-
-				// groupBy
-				//g.Return(g.Exprer("error")),
+				doCode,
 			))
 	return
 }
 
-func (c *Config) SnippetGetData(f *g.File) (snippets []g.Snippet) {
+func (c *Config) SnippetGetData(f *g.File, op string) (snippets []g.Snippet) {
+	var doCode g.Snippet
+	switch op {
+	case "filter":
+		doCode = g.Return(g.Ident("res"))
+	case "map":
+		doCode = g.Return(g.Exprer("nil"),
+			g.Call(f.Use(pkgErrors, "New"), g.Valuer("the value does not support")),
+		)
+	case "groupBy":
+		doCode = g.Return(g.Exprer("error"))
+	}
+
 	snippets = append(snippets, g.Assign(
 		g.Ident("ret"), g.Ident("ok"),
 	).By(
@@ -121,88 +136,80 @@ func (c *Config) SnippetGetData(f *g.File) (snippets []g.Snippet) {
 		g.If(g.Exprer("!ok")).Do(
 			g.CallWith(g.Ref(g.Ident("l"), g.Ident("Error")), g.Ident("err")),
 
-			// filter
-			//g.Return(g.Ident("res")),
-
-			// map
-			g.Return(g.Exprer("nil"),
-				g.Call(f.Use(pkgErrors, "New"), g.Valuer("the value does not support")),
-			),
-
-			// groupBy
-			//g.Return(g.Exprer("error")),
+			doCode,
 		))
 	return
 }
 
-func (c *Config) SnippetOutDeser(f *g.File) (snippets []g.Snippet) {
-	// filter func
-	//snippets = append(snippets, g.Switch(g.Call(f.Use("strings", "ToLower"), g.Ident("ret"))).
-	//	When(
-	//		g.CaseClause(g.Valuer("true")).Do(
-	//			g.Assign(g.Ident("res")).By(g.True),
-	//		),
-	//		g.CaseClause(g.Valuer("false")).Do(
-	//			g.Assign(g.Ident("res")).By(g.False),
-	//		),
-	//		g.CaseClause().Do(
-	//			g.CallWith(
-	//				g.Ref(g.Ident("l"), g.Ident("Warn")),
-	//				g.Call(f.Use(pkgErrors, "New"), g.Valuer("the value does not support"))),
-	//		),
-	//	))
-
-	// map func
-	snippets = append(snippets, g.Assign(
-		g.Ident("err"),
-	).By(
-		g.Call(f.Use("json", "Unmarshal"), g.Ident("ret"), g.Ident("res")),
-	),
-	)
-
-	// groupBy func
-	//snippets = append(snippets, g.Assign(
-	//	g.Ident("res"),
-	//).By(
-	//	g.Casting(g.String, g.Ident("ret")),
-	//),
-	//)
+func (c *Config) SnippetOutDeser(f *g.File, op string) (snippets []g.Snippet) {
+	switch op {
+	case "filter":
+		snippets = append(snippets, g.Switch(g.Call(f.Use("strings", "ToLower"), g.Ident("ret"))).
+			When(
+				g.CaseClause(g.Valuer("true")).Do(
+					g.Assign(g.Ident("res")).By(g.True),
+				),
+				g.CaseClause(g.Valuer("false")).Do(
+					g.Assign(g.Ident("res")).By(g.False),
+				),
+				g.CaseClause().Do(
+					g.CallWith(
+						g.Ref(g.Ident("l"), g.Ident("Warn")),
+						g.Call(f.Use(pkgErrors, "New"), g.Valuer("the value does not support"))),
+				),
+			))
+	case "map":
+		snippets = append(snippets, g.Assign(
+			g.Ident("err"),
+		).By(
+			g.Call(f.Use("json", "Unmarshal"), g.Ident("ret"), g.Ident("res")),
+		),
+		)
+	case "groupBy":
+		snippets = append(snippets, g.Assign(
+			g.Ident("res"),
+		).By(
+			g.Casting(g.String, g.Ident("ret")),
+		),
+		)
+	}
 
 	return
 }
 
-func (c *Config) SnippetReturn() (snippets []g.Snippet) {
-	// filter func
-	//snippets = append(snippets, g.Return(g.Ident("res")))
+func (c *Config) SnippetReturn(op string) (snippets []g.Snippet) {
+	switch op {
+	case "filter":
+		snippets = append(snippets, g.Return(g.Ident("res")))
+	case "map":
+		snippets = append(snippets, g.Return(g.Ident("res"), g.Ident("err")))
+	case "groupBy":
+		snippets = append(snippets, g.Return(g.Ident("res")))
+	}
 
-	// map func
-	snippets = append(snippets, g.Return(g.Ident("res"), g.Ident("err")))
-
-	// groupBy func
-	//snippets = append(snippets, g.Return(g.Ident("res")))
 	return
 }
 
 func (c *Config) SnippetFilterFunc(f *g.File) g.Snippet {
 	var snippets []g.Snippet
-	snippets = append(snippets, c.SnippetCommVar(f)...)
+	snippets = append(snippets, c.SnippetCommVar(f, "filter")...)
 	snippets = append(snippets, c.SnippetInDeser(f)...)
-	snippets = append(snippets, c.SnippetInvokeWasm(f)...)
-	snippets = append(snippets, c.SnippetGetData(f)...)
-	snippets = append(snippets, c.SnippetOutDeser(f)...)
-	snippets = append(snippets, c.SnippetReturn()...)
+	snippets = append(snippets, c.SnippetInvokeWasm(f, "filter")...)
+	snippets = append(snippets, c.SnippetGetData(f, "filter")...)
+	snippets = append(snippets, c.SnippetOutDeser(f, "filter")...)
+	snippets = append(snippets, c.SnippetReturn("filter")...)
 
 	return g.Func(g.Var(g.Interface(), "input")).Named("FilterFunc").Return(g.Var(g.Bool)).Do(snippets...)
 }
 
 func (c *Config) SnippetMapFunc(f *g.File) g.Snippet {
 	var snippets []g.Snippet
-	snippets = append(snippets, c.SnippetCommVar(f)...)
+	snippets = append(snippets, c.SnippetCommVar(f, "map")...)
 	snippets = append(snippets, c.SnippetInDeser(f)...)
-	snippets = append(snippets, c.SnippetInvokeWasm(f)...)
-	snippets = append(snippets, c.SnippetGetData(f)...)
-	snippets = append(snippets, c.SnippetOutDeser(f)...)
-	snippets = append(snippets, c.SnippetReturn()...)
+	snippets = append(snippets, c.SnippetInvokeWasm(f, "map")...)
+	snippets = append(snippets, c.SnippetGetData(f, "map")...)
+	snippets = append(snippets, c.SnippetOutDeser(f, "map")...)
+	snippets = append(snippets, c.SnippetReturn("map")...)
 
 	return g.Func(g.Var(g.Type(f.Use("context", "Context")), "ctx"),
 		g.Var(g.Interface(), "input")).Named("MapFunc").Return(g.Var(g.Interface()), g.Var(g.Error)).Do(snippets...)
@@ -210,12 +217,12 @@ func (c *Config) SnippetMapFunc(f *g.File) g.Snippet {
 
 func (c *Config) SnippetGroupByKey(f *g.File) g.Snippet {
 	var snippets []g.Snippet
-	snippets = append(snippets, c.SnippetCommVar(f)...)
+	snippets = append(snippets, c.SnippetCommVar(f, "groupBy")...)
 	snippets = append(snippets, c.SnippetInDeser(f)...)
-	snippets = append(snippets, c.SnippetInvokeWasm(f)...)
-	snippets = append(snippets, c.SnippetGetData(f)...)
-	snippets = append(snippets, c.SnippetOutDeser(f)...)
-	snippets = append(snippets, c.SnippetReturn()...)
+	snippets = append(snippets, c.SnippetInvokeWasm(f, "groupBy")...)
+	snippets = append(snippets, c.SnippetGetData(f, "groupBy")...)
+	snippets = append(snippets, c.SnippetOutDeser(f, "groupBy")...)
+	snippets = append(snippets, c.SnippetReturn("groupBy")...)
 
 	return g.Func(g.Var(g.Type(f.Use("rxgo", "Item")), "item")).
 		Named("GroupByKey").Return(g.Var(g.String)).Do(snippets...)
