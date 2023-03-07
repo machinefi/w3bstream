f77d198c90ef414f0c9075bfd668d41877ece466
sincos <birdyfj@gmail.com>
2023-03-07 16:45:53 +0800

    fix: mqtt pub qos default 0

--- pkg/types/wasm/wasm_config_mqtt_broker.go
+++ pkg/types/wasm/wasm_config_mqtt_broker.go
@@ -122,7 +122,7 @@ func (b *MqttBroker) PublishWithTopic(ctx context.Context, topic string, payload
 		"app", types.MustAppletFromContext(ctx).Name,
 	)
 	l.Info("start send")
-	if tok := b.cli.Publish(topic, byte(enums.MQTT_QOS__ONLY_ONCE), false, payload); tok.Wait() && tok.Error() != nil {
+	if tok := b.cli.Publish(topic, byte(enums.MQTT_QOS__ONCE), false, payload); tok.Wait() && tok.Error() != nil {
 		l.Error(tok.Error())
 		return tok.Error()
 	}
