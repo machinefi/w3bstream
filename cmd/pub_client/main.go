623111a15114a8e37891a9eb7752f717c7bdf1b1
sincos <birdyfj@gmail.com>
2023-05-08 21:26:50 +0800

    feat(transporter): add mqtt ack

--- cmd/pub_client/main.go
+++ cmd/pub_client/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
+	"path"
 	"time"
 
+	mqtt "github.com/eclipse/paho.mqtt.golang"
 	"github.com/golang/protobuf/proto"
 	"github.com/google/uuid"
+	"github.com/machinefi/w3bstream/pkg/modules/event"
 
 	confapp "github.com/machinefi/w3bstream/pkg/depends/conf/app"
 	conflog "github.com/machinefi/w3bstream/pkg/depends/conf/log"
@@ -15,7 +19,7 @@ import (
 )
 
 var (
-	mqtt   = &confmqtt.Broker{}
+	broker = &confmqtt.Broker{}
 	logger = conflog.Std()
 
 	App *confapp.Ctx
@@ -27,35 +31,43 @@ func init() {
 		confapp.WithLogger(logger),
 		confapp.WithRoot("."),
 	)
-	App.Conf(mqtt)
+	App.Conf(broker)
 
 	flag.StringVar(&id, "id", "", "publish client id")
 	flag.StringVar(&topic, "topic", "", "publish topic")
 	flag.StringVar(&token, "token", "", "publish token")
 	flag.StringVar(&data, "data", "", "payload data")
+	flag.StringVar(&seq, "seq", "", "message sequence")
 	flag.Parse()
 }
 
 var (
-	id      string
-	data    string
-	topic   string
-	token   string
-	payload string
-	raw     []byte
+	id    string
+	data  string
+	topic string
+	token string
+	raw   []byte
+	seq   string
+	msg   *eventpb.Event
 )
 
 func init() {
+	if seq == "" {
+		seq = uuid.NewString()
+	}
+
 	var err error
-	raw, err = proto.Marshal(&eventpb.Event{
+
+	msg = &eventpb.Event{
 		Header: &eventpb.Header{
 			Token:   token,
 			PubTime: time.Now().UTC().UnixMicro(),
-			EventId: uuid.NewString(),
+			EventId: seq,
 		},
 		Payload: []byte(data),
-	})
+	}
 
+	raw, err = proto.Marshal(msg)
 	if err != nil {
 		panic(err)
 	}
@@ -65,7 +77,7 @@ func main() {
 	if id == "" {
 		id = uuid.NewString()
 	}
-	c, err := mqtt.Client(id)
+	c, err := broker.Client(id)
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -75,4 +87,23 @@ func main() {
 		fmt.Println(err)
 		return
 	}
+
+	rspChannel := path.Join(topic, seq)
+
+	err = c.WithTopic(rspChannel).Subscribe(func(cli mqtt.Client, msg mqtt.Message) {
+		rsp := &event.EventRsp{}
+		if err = json.Unmarshal(msg.Payload(), rsp); err != nil {
+			fmt.Println(err)
+		}
+		ack, err := json.MarshalIndent(rsp, "", "  ")
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println(string(ack))
+	})
+	if err != nil {
+		fmt.Println(err)
+	}
+	time.Sleep(time.Second * 3)
+	_ = c.WithTopic(rspChannel).Unsubscribe()
 }
