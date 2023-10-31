f5b40947ad757d0e06b583516f7d56552f12b1b9
huangzhiran <30522704+huangzhiran@users.noreply.github.com>
2023-10-31 12:28:14 +0800

    fix(operator): add ed25519 key pre check (#772)

--- cmd/srv-applet-mgr/apis/operator/pos.go
+++ cmd/srv-applet-mgr/apis/operator/pos.go
@@ -2,7 +2,10 @@ package operator
 
 import (
 	"context"
+	"crypto/ed25519"
+	"fmt"
 
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto"
 
 	"github.com/machinefi/w3bstream/cmd/srv-applet-mgr/apis/middleware"
@@ -23,6 +26,11 @@ func (r *CreateOperator) Output(ctx context.Context) (interface{}, error) {
 	if r.Type == enums.OPERATOR_KEY__ECDSA {
 		if _, err := crypto.HexToECDSA(r.PrivateKey); err != nil {
 			return nil, status.InvalidPrivateKey.StatusErr().WithDesc(err.Error())
+		}
+	} else {
+		b := common.FromHex(r.PrivateKey)
+		if len(b) != ed25519.PrivateKeySize {
+			return nil, status.InvalidPrivateKey.StatusErr().WithDesc(fmt.Sprintf("invalid key size, expect %d, get %d", ed25519.PrivateKeySize, len(b)))
 		}
 	}
 
