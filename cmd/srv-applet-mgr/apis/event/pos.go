dcf350bb9b2c92acd455eedc0e1d72204d3b555c
sincos <birdyfj@gmail.com>
2024-02-20 15:27:56 +0800

    fix(event): fix event request permission

--- cmd/srv-applet-mgr/apis/event/pos.go
+++ cmd/srv-applet-mgr/apis/event/pos.go
@@ -48,8 +48,9 @@ func (r *HandleEvent) Output(ctx context.Context) (interface{}, error) {
 			return nil, status.InvalidDataPushShouldAccount
 		}
 		ctx = ca.WithAccount(ctx)
-		if ca.Role != enums.ACCOUNT_ROLE__ADMIN && ca.AccountID == prj.AccountID {
-			return nil, status.NoProjectPermission
+		if ca.Role != enums.ACCOUNT_ROLE__ADMIN && ca.AccountID != prj.AccountID {
+			return nil, status.NoProjectPermission.StatusErr().
+				WithDesc("data push request should have account permission")
 		}
 		reqs := event.DataPushReqs{}
 		err = json.Unmarshal(r.Payload.Bytes(), &reqs)
@@ -65,6 +66,10 @@ func (r *HandleEvent) Output(ctx context.Context) (interface{}, error) {
 	pub, ok := middleware.MaybePublisher(ctx)
 	if !ok {
 		return nil, status.InvalidDataPushShouldPublisher
+	}
+	if pub.ProjectID != prj.ProjectID {
+		return nil, status.NoProjectPermission.StatusErr().
+			WithDesc("publisher request should have project permission")
 	}
 	ctx = types.WithPublisher(ctx, pub.Publisher)
 	return event.Create(ctx, &r.EventReq)
