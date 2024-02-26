88b3159c80714db4f933c64bec5e1d8ade9de18c
sincos <birdyfj@gmail.com>
2024-02-26 22:50:48 +0800

    fix: project wl list init

--- cmd/srv-applet-mgr/global/config.go
+++ cmd/srv-applet-mgr/global/config.go
@@ -107,8 +107,8 @@ func init() {
 		Risc0Config:      &types.Risc0Config{},
 		Mq:               TaskMgr,
 		MaxWasmFuel:      1024 * 1024 * 1024,
-		ProjectBlackList: []types.SFID{},
-		ProjectWhiteList: []types.SFID{},
+		ProjectBlackList: []types.SFID{0},
+		ProjectWhiteList: []types.SFID{0},
 	}
 
 	name := os.Getenv(consts.EnvProjectName)
